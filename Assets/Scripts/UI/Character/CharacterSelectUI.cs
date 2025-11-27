8eb5716cafbd97462053ad43a166f6ee2cd9fb0b
minFe17 <vvcx1234@naver.com>
2025-11-27 13:03:36 +0900

    씬 버그

--- Assets/Scripts/UI/Character/CharacterSelectUI.cs
+++ Assets/Scripts/UI/Character/CharacterSelectUI.cs
@@ -100,7 +100,7 @@ public class CharacterSelectUI : MonoBehaviourPunCallbacks
                 return;
         }
         if (PhotonNetwork.IsMasterClient)
-            PhotonNetwork.LoadLevel("SelectCharacterScene");
+            PhotonNetwork.LoadLevel("IngameScene");
     }
 
     #region UI Event
