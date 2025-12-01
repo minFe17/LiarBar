884cf8d23e11c1317e271459b787949b5089db53
Subong <rnjstnqls323@naver.com>
2025-12-01 15:06:57 +0900

    voice 테스트중

--- Assets/Scripts/UI/Character/CharacterSelectUI.cs
+++ Assets/Scripts/UI/Character/CharacterSelectUI.cs
@@ -86,7 +86,7 @@ public class CharacterSelectUI : MonoBehaviourPunCallbacks
 
     void CheckAllPlayerReady()
     {
-        if (PhotonNetwork.PlayerList.Length != 2) //멀티 인원
+        if (PhotonNetwork.PlayerList.Length != 3) //멀티 인원
             return;
 
         for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
--- Assets/Scripts/Voice/VoiceManager.cs
+++ Assets/Scripts/Voice/VoiceManager.cs
@@ -1,6 +1,8 @@
 using Photon.Voice.Unity;
 using Photon.Voice.PUN;
 using Photon.Pun;
+using UnityEngine;
+using Utils;
 
 
 public class VoiceManager : MonoBehaviourPunCallbacks
@@ -19,11 +21,36 @@ public class VoiceManager : MonoBehaviourPunCallbacks
         _isOnVoice = isOn;
         _recorder.TransmitEnabled = _isOnVoice;
     }
+
     public override void OnJoinedRoom()
     {
+        Debug.Log("VoiceManager OnJoinedRoom 호출됨!");
         if (PunVoiceClient.Instance != null && !PunVoiceClient.Instance.Client.InRoom)
         {
             PunVoiceClient.Instance.ConnectAndJoinRoom();
         }
     }
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        Debug.Log("Room Created: " + PhotonNetwork.CurrentRoom.Name);
+
+        // Voice 연결
+        if (SimpleSingleton<VoiceManager>.Instance != null)
+        {
+            SimpleSingleton<VoiceManager>.Instance.OnJoinedRoom();
+        }
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        // PunVoiceClient가 이미 있으면 연결
+        var voiceClient = PunVoiceClient.Instance;
+        if (voiceClient != null)
+        {
+            DontDestroyOnLoad(voiceClient.gameObject);
+        }
+    }
+
 }
