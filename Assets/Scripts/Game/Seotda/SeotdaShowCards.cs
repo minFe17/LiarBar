e1354e2441e7fa6c9e2d5b1ad01a9e20d9094be7
Subong <rnjstnqls323@naver.com>
2025-12-10 13:51:51 +0900

    애니메이션 수정

--- Assets/Scripts/Game/Seotda/SeotdaShowCards.cs
+++ Assets/Scripts/Game/Seotda/SeotdaShowCards.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using Photon.Realtime;
 
 public class SeotdaShowCards : MonoBehaviour
 {
@@ -11,9 +12,10 @@ public class SeotdaShowCards : MonoBehaviour
 
     private Transform _playerLeftHand;
     private List<GameObject> _myCards;
+    private List<(Player player, Transform hand)> _playerLeftHands;
     private int _cardCount = 0;
 
-    private Vector3 _originalScale = new Vector3(0.05f, 0.05f, 0.05f);
+    private Vector3 _originalScale = new Vector3(0.03f, 0.03f, 0.03f);
 
     private void Start()
     {
@@ -34,8 +36,22 @@ public class SeotdaShowCards : MonoBehaviour
     }
     private void UpdateCards()
     {
-        //상하좌우 포지션 먼저 맞춰주기 (로테이션도)
+        //상하좌우 포지션 먼저 맞춰주기 (로테이션도) => 포지션 손위치에 걍 박으면됨 로테이션만 맞춰주기
         //카드 증가하는것도 상-> x감소 하->x증가 좌->z감소 우 ->z증가 이렇게 맞춰줘야됨.
+
+        Quaternion[] rotations = {
+            Quaternion.Euler(0, 0, 0),
+            Quaternion.Euler(0, 180, 0),
+            Quaternion.Euler(0, 270, 0),
+            Quaternion.Euler(0, 90, 0)
+        };
+
+        Vector3[] addCardPosition =
+        {
+            new Vector3()
+        };
+
+
         Vector3 position = _playerLeftHand.position + new Vector3(SPACE_VALUE*(_cardCount+1), SPACE_VALUE, 0);
         _myCards[_cardCount].gameObject.transform.localScale = _originalScale;
         _myCards[_cardCount].gameObject.transform.position = position;
@@ -47,6 +63,7 @@ public class SeotdaShowCards : MonoBehaviour
     private void FindPlayerLeftHand()
     {
         _playerLeftHand = MyPlayer.local.LeftHand;
+        _playerLeftHands = MyPlayer.playerLeftHands;
     }
 
     private void FindCards()
